18761e79ee88fceca8765dca0e9ec7d0049560d4
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-29 20:48:44 +0100

    Make code readable.

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from fileProcessingUtils import process
 from uiUtils import create_economy_data_plot
 
 create_economy_data_plot(
-    process('forint_inflation_rate.json'),
-    "Forint Inflation",
-    "Year",
-    "Rate"
+    source=process('forint_inflation_rate.json'),
+    title="Forint Inflation",
+    xlabel="Year",
+    ylabel="Rate"
 )
 
 create_economy_data_plot(
-    process('forint_interest_rate.json'),
-    "Forint Interest Rate",
-    "Year",
-    "Value",
+    source=process('forint_interest_rate.json'),
+    title="Forint Interest Rate",
+    xlabel="Year",
+    ylabel="Value",
 )
