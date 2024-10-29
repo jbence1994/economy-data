291c167a295e756b2f07977f642fcb31c812e15f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-29 20:57:48 +0100

    Restructure sources folder.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from data_processing.file_processor import process
 from ui.ui_utils import create_economy_data_plot
 
 create_economy_data_plot(
-    source=process('sources/inflation/forint_inflation_rate.json'),
+    source=process('sources/forint_inflation_rate.json'),
     title="Forint Inflation",
     xlabel="Year",
     ylabel="Rate"
 )
 
 create_economy_data_plot(
-    source=process('sources/interest_rates/forint_interest_rate.json'),
+    source=process('sources/forint_interest_rate.json'),
     title="Forint Interest Rate",
     xlabel="Year",
     ylabel="Value",
