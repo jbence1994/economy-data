1bb6b5a9fd552c5e54d0d0ff970b92fd15287a68
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-29 20:50:26 +0100

    Put JSON files to subfolders.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fileProcessingUtils import process
 from uiUtils import create_economy_data_plot
 
 create_economy_data_plot(
-    source=process('forint_inflation_rate.json'),
+    source=process('data_sources/inflation/forint_inflation_rate.json'),
     title="Forint Inflation",
     xlabel="Year",
     ylabel="Rate"
 )
 
 create_economy_data_plot(
-    source=process('forint_interest_rate.json'),
+    source=process('data_sources/interest_rates/forint_interest_rate.json'),
     title="Forint Interest Rate",
     xlabel="Year",
     ylabel="Value",
