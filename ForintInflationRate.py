87b17563259e2df71ec2e741d6c09b4ae7bfdad4
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-29 16:17:05 +0100

    Add getters.

--- ForintInflationRate.py
+++ ForintInflationRate.py
@@ -4,5 +4,17 @@ class ForintInflationRate:
         self._month = month
         self._rate = rate
 
+    @property
+    def year(self):
+        return self._year
+
+    @property
+    def month(self):
+        return self._month
+
+    @property
+    def rate(self):
+        return self._rate
+
     def __str__(self):
         return f'{self._year} {self._month} {self._rate}'
--- ForintInterestRate.py
+++ ForintInterestRate.py
@@ -3,5 +3,13 @@ class ForintInterestRate:
         self._date = date
         self._rate = rate
 
+    @property
+    def date(self):
+        return self._date
+
+    @property
+    def rate(self):
+        return self._rate
+
     def __str__(self):
         return f'{self._rate} {self._date}'
