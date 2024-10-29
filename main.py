55e8fdc7f6030aad1c48d99a3aabdbfa19a296b8
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-10-29 20:25:04 +0100

    Refactor code by removing duplications.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fileProcessingUtils import process
-from uiUtils import display_inflation_rate, display_interest_rate
+from uiUtils import create_economy_data_plot
 
-display_inflation_rate(process('forint_inflation_rate.json'))
-display_interest_rate(process('forint_interest_rate.json'))
+create_economy_data_plot(
+    process('forint_inflation_rate.json'),
+    "Forint Inflation",
+    "Year",
+    "Rate"
+)
+
+create_economy_data_plot(
+    process('forint_interest_rate.json'),
+    "Forint Interest Rate",
+    "Year",
+    "Value",
+)
--- uiUtils.py
+++ uiUtils.py
@@ -1,48 +1,24 @@
 from matplotlib import pyplot
 
 
-def display_inflation_rate(source):
-    inflation_dates = []
-    inflation_rates = []
+def create_economy_data_plot(source, title, xlabel, ylabel):
+    dates = []
+    rates = []
 
-    for inflation_rate in source:
-        inflation_dates.append(inflation_rate.date)
-        inflation_rates.append(inflation_rate.rate)
+    for economy_data in source:
+        dates.append(economy_data.date)
+        rates.append(economy_data.rate)
 
-    pyplot.plot(inflation_rates, marker='o')
+    pyplot.plot(rates, marker='o')
 
-    for i, rate in enumerate(inflation_rates):
+    for i, rate in enumerate(rates):
         pyplot.text(i, rate, f"{rate}", ha='right', va='top')
 
-    pyplot.xticks(ticks=range(len(inflation_dates)), labels=inflation_dates, rotation=45)
+    pyplot.xticks(ticks=range(len(dates)), labels=dates, rotation=45)
 
-    pyplot.title("Forint Inflation")
-    pyplot.xlabel("Year")
-    pyplot.ylabel("Rate")
-
-    pyplot.tight_layout()
-
-    pyplot.show()
-
-
-def display_interest_rate(source):
-    interest_rate_dates = []
-    interest_rate_values = []
-
-    for interest_rate in source:
-        interest_rate_dates.append(interest_rate.date)
-        interest_rate_values.append(interest_rate.rate)
-
-    pyplot.plot(interest_rate_values, marker='o')
-
-    for i, rate in enumerate(interest_rate_values):
-        pyplot.text(i, rate, f"{rate}", ha='right', va='bottom')
-
-    pyplot.xticks(ticks=range(len(interest_rate_dates)), labels=interest_rate_dates, rotation=45)
-
-    pyplot.title("Forint Interest Rate")
-    pyplot.xlabel("Year")
-    pyplot.ylabel("Value")
+    pyplot.title(title)
+    pyplot.xlabel(xlabel)
+    pyplot.ylabel(ylabel)
 
     pyplot.tight_layout()
 
